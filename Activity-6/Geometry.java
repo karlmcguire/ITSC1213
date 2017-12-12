e0ddddf5b5022f7d438f64cfe6ef067cf81f4776
Karl McGuire <karl@karlmcguire.com>
2017-12-12 09:36:01 -0500

    documentation

--- Activity-6/Geometry.java
+++ Activity-6/Geometry.java
@@ -1,31 +1,48 @@
+/**
+ * Geometry demonstrates my extensive knowledge of the 'final' keyword in Java.
+ *
+ * @author Karl McGuire
+ * @version 12/12/17
+ */
 public class Geometry {
-    static final double PI = 3.14159;
-    
+	// PI is a (short) representation of the PI constant for use in calculating
+	// the area of circles	
+	static final double PI = 3.14159;
 
-    public static double circleArea(double radius) {
-        if(radius <= 0) {
-            System.err.println("non positive");
-            return 0;
-        }
+	/**
+	 * circleArea calculates the area of a circle given the radius.
+	 *
+	 * @param radius is the radius of the circle
+	 * @return area 
+	 */
+	public static double circleArea(double radius) {
+		// if less then 0, just return 0
+		return (radius <= 0) ? 0 : PI * (radius * radius);
+	}
 
-        return PI * (radius * radius);  
-    }
+	/**
+	 * rectangleArea calculates the area of a rectangle given the length and 
+	 * width.
+	 *
+	 * @param length is the length of the rectangle
+	 * @param width is the width of the rectangle
+	 * @return area of triangle
+	 */
+	public static double rectangleArea(double length, double width) {
+		// if less then 0, just return 0
+		return (length <= 0 || width <= 0) ? 0 : length * width;
+	}
 
-    public static double rectangleArea(double length, double width) {
-        if(length <= 0 || width <= 0) {
-            System.err.println("non positive");
-            return 0;
-        }
-        
-        return length * width;
-    }
-
-    public static double triangleArea(double base, double height) {
-        if(base <= 0 || height <= 0) {
-            System.err.println("non positive");
-            return 0;
-        }
-
-        return (0.5) * (base * height);
-    }
+	/**
+	 * triangleArea calculates the area of a triangle given the base length and
+	 * height.
+	 *
+	 * @param base is the base length of the triangle
+	 * @param height is the height of the triangle
+	 * @return area of triangle
+	 */
+	public static double triangleArea(double base, double height) {
+		// if less then 0, just return 0
+		return (base <= 0 || height <= 0) ? 0 : 0.5 * (base * height);
+	}
 }
